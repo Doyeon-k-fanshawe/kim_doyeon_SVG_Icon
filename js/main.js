dd9d630d1df10666d61d2ab30e7eaabcefd94ea9
Doyeon <d_kim120803@fanshaweonline.ca>
2022-01-29 14:04:42 -0500

    building html and css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 (() => {
   let theHeading = document.querySelector(".main-heading"),
       theIcons = document.querySelectorAll(".icon");
+      theNames = document.querySelectorAll(".name")
 
   function logElement() {
     console.log('clicked on an element:', this.id);
@@ -9,4 +10,6 @@
   theHeading.addEventListener("click", logElement);
 
   theIcons.forEach(icon => icon.addEventListener("click", logElement));
+
+  theNames.forEach(name => name.addEventListener("click", logElement));
 })();
